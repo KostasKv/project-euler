1b356abaeaede182e5aa6b41dfd5f71f92ec763a
KostasKv <kostas@pttgroup.co.uk>
2025-03-23 12:51:22 +0000

    :brain:

---
+++ 2-Even_Fibonacci_Numbers.cpp
@@ -0,0 +1,28 @@
+// https://projecteuler.net/problem=2
+// Output: 4613732
+// Time: 0.05s
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int even_fibonacci(int n, int memo[]) {
+    if (n == 0) return 0;
+    if (n == 1) return 2;
+    if (memo[n] != -1) return memo[n];
+
+    return memo[n] = 4 * even_fibonacci(n - 1, memo) + even_fibonacci(n - 2, memo);
+}
+
+int main() {
+    int memo[100000];
+    fill_n(memo, 100000, -1);
+
+    const int MAX = 4000000;
+    int sum = 0;
+
+    for (int i = 0, fib; (fib = even_fibonacci(i, memo)) <= MAX ; i += 1) {
+        sum += fib;
+    }
+
+    cout << sum << endl;
+}
