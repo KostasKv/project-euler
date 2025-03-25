fa32f05b00010bf26c152fa2e5afa3cffb38644c
KostasKv <kostas@pttgroup.co.uk>
2025-03-25 13:43:03 +0000

    :brain:

---
+++ 1-Multiples_of_3_or_5.cpp
@@ -0,0 +1,17 @@
+// https://projecteuler.net/problem=1
+// Output: 233168
+// Time: 0.65s
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int sumMultiplesOfUpTo(int k, int max) {
+    int largest_multiple = max - (max % k);
+    int number_of_terms_in_sum = largest_multiple / k;
+    return (k + largest_multiple) * number_of_terms_in_sum  / 2;
+}
+
+int main() {
+    int n = 999;
+    cout << sumMultiplesOfUpTo(3, n) + sumMultiplesOfUpTo(5, n) - sumMultiplesOfUpTo(15, n) << endl;
+}
--- 1-Multiples_of_3_or_5.py
+++
@@ -1,4 +0,0 @@
-# https://projecteuler.net/problem=1
-# Output: 233168
-# Time: 0.06s
-print(sum(set(range(3, 1000, 3)) | set(range(5, 1000, 5))))
